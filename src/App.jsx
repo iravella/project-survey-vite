e6d26f5ebd4472ed51e2dc49de19b2baa8c16bfd
Emelie Segell <segell.emelie@gmail.com>
2023-10-25 22:34:43 +0200

    first tries at components and something in the DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { QuestionOne } from "./components/QuestionOne";
+
+const isTrue = false;
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <form>
+      <div className="container">
+        <h2>This is the survey!</h2>
+        <div className="question-container">
+          <div>Question</div>
+          <QuestionOne />
+
+          <div className="footer">
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
+
+//one component per question. Components should keep the heading and the buttons/select/checkboxes
+//Summary has own design
---
+++ src/components/QuestionOne.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const QuestionOne = () => {
+  return <div>QuestionOne</div>;
+};
